8e30bab1d1b037842f268683af8ecb2f0b64d95f
Niko Osolnik <prvi@ocene.si>
2025-12-30 08:16:45 +0000

    Add initial test user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create!(
+  email: "mare@ocene.si",
+  password: "123kop",
+  password_confirmation: "123kop",
+  name: "Mare"
+)
